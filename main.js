edfaba1c10f238d02a1bc300f01de9c1fc29cce7
Marcos <marcv56@hotmail.com>
2020-01-24 01:26:27 -0500

    it works

--- main.js
+++ main.js
@@ -30,13 +30,61 @@ var updateDisplayVal = (clickObj) => {
   displayValElement.innerText = displayVal;
 }
 
+var performOperation = (clickObj) => {
+  var operator = clickObj.target.innerText
+
+  switch (operator) {
+    case '+':
+    pendingVal = displayVal;
+    displayVal = '0';
+    displayValElement.innerText = displayVal;
+    evalStringArray.push(pendingVal);
+    evalStringArray.push('+');
+      break;
+
+      case '-':
+      pendingVal = displayVal;
+      displayVal = '0';
+      displayValElement.innerText = displayVal;
+      evalStringArray.push(pendingVal);
+      evalStringArray.push('-');
+      break;
+
+case 'x':
+pendingVal = displayVal;
+displayVal = '0';
+displayValElement.innerText = displayVal;
+evalStringArray.push(pendingVal);
+evalStringArray.push('*');
+break;
+
+case '÷':
+pendingVal = displayVal;
+displayVal = '0';
+displayValElement.innerText = displayVal;
+evalStringArray.push(pendingVal);
+evalStringArray.push('/');
+break;
+
+case '=':
+evalStringArray.push(displayVal);
+var evaluation = eval(evalStringArray.join(''));
+displayVal = evaluation + '';
+displayValElement.innerText = displayVal;
+evalStringArray = [];
+break;
+  default:
+  break;
+  }
+}
+
 
 for (let i = 0; i < calcNumBtns.length; i++) {
   calcNumBtns[i].addEventListener('click', updateDisplayVal, false)
 }
-// for (let i = 0; < calcOperatorBtns.length; i++) {
-//     calcOperatorBtns[i].addEventListener('click', performOperation , false)
-// }
+for (let i = 0; i < calcOperatorBtns.length; i++) {
+    calcOperatorBtns[i].addEventListener('click', performOperation , false)
+}
 
 clearBtn.onclick = () => {
   displayVal = '0';
@@ -48,4 +96,5 @@ clearBtn.onclick = () => {
 decimalBtn.onclick = () => {
   if(!displayVal.includes('.'))
   displayVal += '.';
+  displayValElement.innerText = displayVal;
 }
